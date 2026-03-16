9ae2664936df467cff5a60f617985cd759395745
Santiago-Lopez07 <s4il.is.a.dev@gmail.com>
2026-03-16 11:00:54 -0500

    primer commit

--- CleanTeeth.Domain/obj/Debug/net10.0/CleanTeeth.Domain.AssemblyInfo.cs
+++ CleanTeeth.Domain/obj/Debug/net10.0/CleanTeeth.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanTeeth.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b1f65128f9a6a36a9da4cd16a4a2b34079752c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3b3970fc7ab7b7a281ba3182c708e0bf9c0a805")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanTeeth.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanTeeth.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
